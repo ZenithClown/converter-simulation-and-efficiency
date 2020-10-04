1e13b8a8de866b8163ae1722f874e7d64ce4c27e
Debmalya Pramanik <DebmalyaPramanik.005@gmail.com>
2020-10-05 01:30:20 +0530

    Updated Figure with Measurement Blocks
    * included ConverterDesign.slx File for Modification and Testing
    
    * updated config.m with all the parameters
    
    * have to update block tags

--- config.m
+++ config.m
@@ -25,30 +25,34 @@ clear data step a i; % Delete unwanted data
 temp = 25; % Constant, but can be built using signal builder
 
 %% PV Array: 1Soltech - 1STH-215-P
-% Max. Power = 213.15 W
+%  with a Max. Power = 213.15 W per Cell
 C_PV = 100e-6; % Capacitance Value across PV Array (annot. as C_PV)
 R_PV = 10e-3;  % Resistance Value across PV Array (annot. as C_PV)
 
-%% RLC Value for DC-DC Converters
-% Boost, Buck-Boost
+%% DC-DC Converters Specifications
 Rs = 0.1;  % Series Resistance
 Rp = 1e-4; % Palallel Resistance
-L  = 5e-3;
-C  = 3330e-6;
 
-% Buck Converter
-Rs_buck = 0.1e-3; % Series Resistance
-Rp_buck = 1e-5;   % Palallel Resistance
-L_buck  = 0.2e-3;
-C_buck  = 3330e-6;
+%  Boost Converter      : bst
+L_bst = 5e-3;
+C_bst = 3330e-6;
 
-% CUK Converter
+%  Buck Converter       : bck
+L_bck  = 0.2e-3;
+C_bck  = 3330e-6;
+
+%  Buck-Boost Converter : bbc
+L_bbc = 0.05;
+C_bbc = 1e-4;
+%  CUK Converter        : cuk
+%  SEPIC Converter      : spc
+
 L1 = 1.73e-3;
 L2 = 57.6e-3;
 C1 = 3.52e-6;
 C2 = 20.05e-9;
 
-% SEPIC Converter
+
 L1_s  = 1.73e-3;
 L2_s  = 57.6e-3;
 C1_s  = 3.52e-6;
@@ -56,4 +60,4 @@ C2_s  = 20.05e-9;
 Cin_s = 3.3e-6;
 
 % Resistive Load
-LOAD = 7;
+LOAD = 50;
