39da8f7ceadd01bd8383d3cc7328a7e7b8c1b8c0
ZenithClown <DebmalyaPramanik.005@gmail.com>
2020-11-11 23:44:46 +0530

    Updated Files

--- GridConnectedDesignConfiguration.m
+++ GridConnectedDesignConfiguration.m
@@ -1,16 +1,11 @@
-clc;
-clear;
-
-%% Model Configuration
-T  = 10.0;  % Simulation Time
-Ts = 10e-6; % Sample Time (fixed-step, discrete)
-
+%% Setting Simulation Constants
+T  = 10;
+Ts = 5e-6;
 %% Reading Solar Direct Radiance Data
 data = readtable('./data/direct_solar_radiation_in_kW_per_m2.csv');
 time = data.TIME;              % Time of the Day
 radiation = data.RADIATION;    % Direct Solar Radiation in kW/m^2
 radiation = radiation .* 1000; % Converted Radiation in W/m^2
-radiation = radiation + 25;
 
 a = 0;
 step = T / length(time);
@@ -21,14 +16,11 @@ end
 
 clear data step a i; % Delete unwanted data
 plot(time,radiation)
+grid on;
 xlabel('Time of the Day');
-ylabel('Direct Solar Radiance kW/m^2');
-title('Distribution of Solar Radiance on a Day (Location: Latitude = 30 deg. South)');
-
-%% Temperature Data
-temp = 25; % Constant, but can be built using signal builder
-
+ylabel('Direct Solar Radiance (kW/m^2)');
+title('Distribution of Direct Solar Radiance on a Day (Location: Latitude = 30 deg. South)');
 %% PV Array: 1Soltech - 1STH-215-P
-% Max. Power = 213.15 W
-C_PV = 100e-6; % Capacitance Value across PV Array (annot. as C_PV)
-R_PV = 10e-3;  % Resistance Value across PV Array (annot. as C_PV)
+
+C_PV = 0.05;  % Capacitance Value across PV Array (annot. as C_PV)
+R_PV = 10e-3; % Resistance Value across PV Array (annot. as C_PV)
